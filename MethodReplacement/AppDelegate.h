e17f33b06b9fbe5eff2e2df2eb983298136d7415
Dario Lencina <darioalessandro@me.com>
2012-11-28 02:19:32 -0500

    Initial Commit

---
+++ MethodReplacement/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MethodReplacement
+//
+//  Created by Dario Lencina on 11/28/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
